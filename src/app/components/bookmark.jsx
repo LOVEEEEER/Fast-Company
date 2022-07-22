09709601227443cc83fe34da83134aa871a87f05
Nikita Demyanenko <nikitademyanenko000@gmail.com>
2022-07-22 19:00:45 +1000

    lesson-4-init-project

--- src/app/components/bookmark.jsx
+++ src/app/components/bookmark.jsx
@@ -1,12 +1,10 @@
 import React from "react";
-
 const BookMark = ({ status, ...rest }) => {
-  console.log(rest);
-  return (
-    <button {...rest}>
-      <i className={"bi bi-bookmark" + (status ? "-heart-fill" : "")}></i>
-    </button>
-  );
+    return (
+        <button {...rest}>
+            <i className={"bi bi-bookmark" + (status ? "-heart-fill" : "")}></i>
+        </button>
+    );
 };
 
 export default BookMark;
--- src/app/components/user.jsx
+++ src/app/components/user.jsx
@@ -2,37 +2,43 @@ import React from "react";
 import Qualitie from "./qualitie";
 import BookMark from "./bookmark";
 const User = ({
-  _id,
-  name,
-  qualities,
-  profession,
-  completedMeetings,
-  rate,
-  bookmark,
-  onToggleBookMark,
-  onDelete,
+    _id,
+    name,
+    qualities,
+    profession,
+    completedMeetings,
+    rate,
+    bookmark,
+    onToggleBookMark,
+    onDelete
 }) => {
-  return (
-    <tr key={_id}>
-      <td>{name}</td>
-      <td>
-        {qualities.map((quality) => (
-          <Qualitie key={quality._id} {...quality} />
-        ))}
-      </td>
-      <td>{profession.name}</td>
-      <td>{completedMeetings}</td>
-      <td>{rate}</td>
-      <td>
-        <BookMark status={bookmark} onClick={() => onToggleBookMark(_id)} />
-      </td>
-      <td>
-        <button className="btn btn-danger" onClick={() => onDelete(_id)}>
-          delete
-        </button>
-      </td>
-    </tr>
-  );
+    return (
+        <tr key={_id}>
+            <td>{name}</td>
+            <td>
+                {qualities.map((quality) => (
+                    <Qualitie key={quality._id} {...quality} />
+                ))}
+            </td>
+            <td>{profession.name}</td>
+            <td>{completedMeetings}</td>
+            <td>{rate}</td>
+            <td>
+                <BookMark
+                    status={bookmark}
+                    onClick={() => onToggleBookMark(_id)}
+                />
+            </td>
+            <td>
+                <button
+                    className="btn btn-danger"
+                    onClick={() => onDelete(_id)}
+                >
+                    delete
+                </button>
+            </td>
+        </tr>
+    );
 };
 
 export default User;
